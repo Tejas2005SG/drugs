863d83758a6804088b13522f3ef131e3badd0bec
Alok-2005 <alokchaturvedi190@gmail.com>
2025-06-07 23:33:50 +0530

    jarvis

--- frontend/src/pages/Chatbot/Chatbot.jsx
+++ frontend/src/pages/Chatbot/Chatbot.jsx
@@ -382,7 +382,7 @@ export default function Chatbot() {
 
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${import.meta.env.VITE_API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDyujm50dHMYvn1V50dDDqcAhgUqCOuUGU`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
