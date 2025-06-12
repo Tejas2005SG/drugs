a2012128cba8f9e973f7b488cffed5ebe6708d76
Tejas2005SG <tbhangale9@gmail.com>
2025-06-12 13:37:28 +0530

    deploy

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,11 @@
-import Redis from "ioredis";
+import { Redis } from '@upstash/redis'
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+
+export const redis = new Redis({
+  url: 'https://immense-goldfish-16178.upstash.io',
+  token: 'AT8yAAIjcDE5YTI1YWJlN2Q0YmE0NTQ3OWFjNTZmYWMwZDViMjdhMHAxMA',
+})
+
