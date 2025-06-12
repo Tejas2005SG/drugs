cd0288582b91e6a07d12b029052ecd1bb87a4eb9
Tejas2005SG <tbhangale9@gmail.com>
2025-06-12 19:29:43 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -85,9 +85,9 @@ app.use("/api/researchPaper", researchPaperRoutes);
 // app.use("/api/jarvis",jarvisRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
