888a7144771627528a276cf8b59ad6ab08499085
Tejas2005SG <tbhangale9@gmail.com>
2025-06-12 14:52:56 +0530

    dely

--- backend/server.js
+++ backend/server.js
@@ -84,12 +84,12 @@ app.use("/api/researchPaper", researchPaperRoutes);
 
 // // app.use("/api/jarvis",jarvisRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectionDb();
