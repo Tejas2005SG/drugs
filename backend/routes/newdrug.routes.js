86072e362031be78af205ea41bb56eef200c7063
Tejas2005SG <tbhangale9@gmail.com>
2025-06-13 10:53:29 +0530

    deploy

--- backend/routes/newdrug.routes.js
+++ backend/routes/newdrug.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { predictDisease,predictTargetProtein, getnewdrug,getSymptoms } from '../controllers/newdrug.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
+
 const router = express.Router();
 
-router.post("/predictDisease/:id",protectRoute,predictDisease);
-router.post("/predictTargetProtein",protectRoute,predictTargetProtein);
-router.get('/getnewdrug/:id',protectRoute, getnewdrug);
-router.get('/symptoms/:id',protectRoute, getSymptoms);
+router.post("/predictDisease/:id",predictDisease);
+router.post("/predictTargetProtein",predictTargetProtein);
+router.get('/getnewdrug/:id', getnewdrug);
+router.get('/symptoms/:id', getSymptoms);
 
 
 export default router;
